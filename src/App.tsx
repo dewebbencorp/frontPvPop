72d8b72dfd5a35921458ea93c8832295d8497a32
emorales1997 <auxdesarrolloweb@bencorp.com.mx>
2024-10-08 17:00:25 -0500

    retuns

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 2000); 
+    }, 3000); 
 
     return () => clearTimeout(timer);
   }, []);
