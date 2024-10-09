e9fae83a4e13f54ea969a51b84cea5accf66b844
LaloBencorp <proyectos2@bencorp.com.mx>
2024-10-09 09:09:44 -0500

    feat: Demo Zustand

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-import '@ionic/react/css/palettes/dark.system.css';
+/* import '@ionic/react/css/palettes/dark.system.css'; */
 
 // App and custom theme
 import './theme/variables.css';
