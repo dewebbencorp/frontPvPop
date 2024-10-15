02eb8c382395294b300c5e66847fdef74332a9ca
dewebbencorp <73552767+dewebbencorp@users.noreply.github.com>
2024-10-15 10:58:00 -0500

    cambios de android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontPvPop',
+  webDir: 'dist'
+};
+
+export default config;
